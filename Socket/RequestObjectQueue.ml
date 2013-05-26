c0bf96345fbf0ab8865bc8f8f8b3d4974faafcb9
Nguyen Le Thanh Dung <nguyentito@gmail.com>
2013-05-26 13:43:02 +0200

    Cosmetic fixes.

--- Socket/RequestObjectQueue.ml
+++ Socket/RequestObjectQueue.ml
@@ -1,84 +1,70 @@
-module type SingleType =
-  sig 
-    type t
-  end
+module type SingleType = sig 
+  type t
+end
 
-module type ObjReqAbsQueue =
-  functor (Obj : SingleType) ->
-    sig
-      type t
+module type ObjReqAbsQueue = functor (Obj : SingleType) -> sig
+  type t
 
-      type slot = 
-        | Obj of Obj.t
-	| Query of (Obj.t -> unit)
+  type slot = Obj of Obj.t
+            | Query of (Obj.t -> unit)
 
-      type queue_state =
-	(* FIXME : choose better names *)
-        | Empty
-	| Normal 
-	| Request
+  (* FIXME : choose better names *)
+  type queue_state = Empty
+                   | Normal 
+                   | Request
 
-      val create : unit -> t
-      val add : slot -> t -> unit
-      val pop : t -> slot
-      val get_state : t -> queue_state
-    end
+  val create : unit -> t
+  val add : slot -> t -> unit
+  val pop : t -> slot
+  val get_state : t -> queue_state
+end
 
-module type ObjReqAbsHashtbl =
-  sig
-    module Queue : ObjReqAbsQueue
+module type ObjReqAbsHashtbl = sig
+  module Queue : ObjReqAbsQueue
 
-    val get_queue : 'a -> 'b
-  end
+  val get_queue : 'a -> 'b
+end
 
-module ObjReqQueue : ObjReqAbsQueue =
-  functor (Obj : SingleType) ->
-    struct
-      type obj = Obj.t
-	
-      type slot = 
-        | Obj of obj
-	| Query of (obj -> unit)
-	  
-      type queue_state =
-	(* FIXME : choose better names *)
-        | Empty
-	| Normal 
-	| Request
+module ObjReqQueue : ObjReqAbsQueue = functor (Obj : SingleType) -> struct
+  type obj = Obj.t
+
+  type slot = Obj of Obj.t
+            | Query of (Obj.t -> unit)
+
+  (* FIXME : choose better names *)
+  type queue_state = Empty
+                   | Normal 
+                   | Request
 
 
-      type t = slot Queue.t
-	
-      let create = Queue.create
-      let add : (slot -> t -> unit) = Queue.add
-      let pop : (t -> slot) = Queue.pop
+  type t = slot Queue.t
+    
+  let create = Queue.create
+  let add : (slot -> t -> unit) = Queue.add
+  let pop : (t -> slot) = Queue.pop
 
-      let get_state q =
-	if Queue.is_empty q then
-	  Empty
-	    
-	else
-	  match Queue.peek q with
-            | Obj _ -> Normal
-	    | Query _ -> Request
+  let get_state q =
+    if Queue.is_empty q
+    then Empty
+    else match Queue.peek q with
+    | Obj _ -> Normal
+    | Query _ -> Request
 
-    end    
-      
-module ObjReqHashtbl (Id : SingleType) (Obj : SingleType) =
-  struct
-    type id = Id.t
-    type obj = Obj.t
+end    
+  
+module ObjReqHashtbl (Id : SingleType) (Obj : SingleType) =struct
+  type id = Id.t
+  type obj = Obj.t
 
-    module Queue = ObjReqQueue (Obj)
+  module Queue = ObjReqQueue (Obj)
 
-    let objects : (id, Queue.t) Hashtbl.t = Hashtbl.create 577
+  let objects : (id, Queue.t) Hashtbl.t = Hashtbl.create 577
 
-    let get_queue id =
-      try Hashtbl.find objects id 
-      with
-        | Not_found ->
-	  let fifo = Queue.create () in
-	  Hashtbl.add objects id fifo;
-	  fifo
+  let get_queue id =
+    try Hashtbl.find objects id 
+    with Not_found ->
+      let fifo = Queue.create () in
+      Hashtbl.add objects id fifo;
+      fifo
 
-  end
+end
--- Socket/Server.ml
+++ Socket/Server.ml
@@ -8,113 +8,111 @@ module type ServerConfig = sig
   val port : int
 end
 
-module type Server = 
-  functor (Cfg : ServerConfig) ->
-    sig
-      val get_addr : unit -> sockaddr
-      val run : unit -> unit
-    end
+module type Server = functor (Cfg : ServerConfig) -> sig
+  val get_addr : unit -> sockaddr
+  val run : unit -> unit
+end
 
 
-module SimpleServer : Server = 
-  functor (Cfg : ServerConfig) ->
-    struct
-      open Thread
+module SimpleServer : Server = functor (Cfg : ServerConfig) -> struct
+  open Thread
 
-      let serv_addr = ADDR_INET (Cfg.ip, Cfg.port)
+  let serv_addr = ADDR_INET (Cfg.ip, Cfg.port)
 
-      let server_socket = handle_unix_error
-        (fun () -> socket PF_INET SOCK_STREAM(* SEQPACKET *) 0) ()
-	
-      (* Utilities *)
-      let fire = create
+  let server_socket = handle_unix_error
+    (fun () -> socket PF_INET SOCK_STREAM(* SEQPACKET *) 0) ()
+    
+  (* Utilities *)
+  let fire = create
 
-      (** Request/objects queue **)
-      module ROHstb = RequestObjectQueue.ObjReqHashtbl (struct type t = int end) (String)
-      open ROHstb.Queue
-
-      let obj_mutex = Mutex.create ()
-
-      (* Request/objects queue utility functions *)
-      (* /!\ This function lock the hashtable mutex /!\ *)
-      let get_queue id =
-        Mutex.lock obj_mutex;
-        ROHstb.get_queue id
+  (** Request/objects queue **)
+  module ROHstb = RequestObjectQueue.ObjReqHashtbl (struct type t = int end) (String)
+  open ROHstb.Queue
 
 
-	
-      let rec server_worker fd =
-	let in_ch = in_channel_of_descr fd in
-	while true do 
-	  let id = input_binary_int in_ch in
-	  let op = input_line in_ch in
-	  match op with
-  	    | "put" ->
-	      let obj = input_line in_ch in
-	      let fifo = get_queue id in
-	      begin
-		match get_state fifo with
-		  | Empty | Normal ->
-		    add (Obj obj) fifo;
-		    Mutex.unlock obj_mutex
-		      
-		  | Request ->
-		    begin
-		      match pop fifo with
-	                | Query f -> f obj
-			| _ -> assert false
-		    end;
+  (* To prevent multiple workers from trying to access the queue
+     at the same time, we introduce a mutex and a function get_queue
+     which tries to lock it before acting on the queue *)
+  let obj_mutex = Mutex.create ()
 
-		    Mutex.unlock obj_mutex
-	      end
-		
-	    | "get" ->
-	      let fifo = get_queue id in
-	      let obj = ref "" in
-	      
-	      (* Get the requested object and lock ourselves if not yet present *)
-	      begin
-		match get_state fifo with
- 		  | Normal -> 
-		    obj := (match pop fifo with
- 		      | Obj s -> s
-		      | _ -> assert false)
-		    ;
-		    Mutex.unlock obj_mutex
-		      
- 		  | Empty | Request ->
-		    let mut = Mutex.create () in
-		    let query = fun s -> obj := s; Mutex.unlock mut in
-		    Mutex.lock mut;
-		    add (Query query) fifo;
-		    
-		    (* FIXME : we should not be interrupted between next 2 instructions *)
-		    Mutex.unlock obj_mutex;
-		    Mutex.lock mut (* Let's purposefully lock ourselves ! *)
-	      end;
-	  
-	      (* Then send the result *)
-	      let out_ch = out_channel_of_descr fd in
-	      output_string out_ch (!obj ^ "\n");
-	      flush out_ch
-		
-	    | _ -> assert false
-        done
+  (* Request/objects queue utility functions *)
+  (* /!\ This function locks the hashtable mutex /!\ *)
+  let get_queue id =
+    Mutex.lock obj_mutex;
+    ROHstb.get_queue id
 
-      let server_main () =
-        handle_unix_error (fun () -> 
-          bind server_socket serv_addr;
-          listen server_socket Cfg.port;) ();
-        while true do 
-          let fd = fst =< accept $ server_socket in
-          ignore (fire server_worker fd) (* FIXME : make a real shutdown *)
-        done
+      
+  let rec server_worker fd =
+    let in_ch = in_channel_of_descr fd in
+    while true do 
+      let id = input_binary_int in_ch in
+      let op = input_line in_ch in
+      match op with
+        | "put" ->
+        let obj = input_line in_ch in
+        let fifo = get_queue id in
+        begin
+          match get_state fifo with
+          | Empty | Normal ->
+            add (Obj obj) fifo;
+            Mutex.unlock obj_mutex
+              
+          | Request ->
+            begin
+              match pop fifo with
+              | Query f -> f obj
+              | _ -> assert false
+            end;
+
+            Mutex.unlock obj_mutex
+        end
           
+      | "get" ->
+        let fifo = get_queue id in
+        let obj = ref "" in
+        
+          (* Get the requested object and lock ourselves if not yet present *)
+        begin
+          match get_state fifo with
+           | Normal -> 
+            obj := (match pop fifo with
+                    | Obj s -> s
+                    | _ -> assert false);
+            Mutex.unlock obj_mutex
+              
+           | Empty | Request ->
+            let mut = Mutex.create () in
+            let query = fun s -> obj := s; Mutex.unlock mut in
+            Mutex.lock mut;
+            add (Query query) fifo;
+            
+            (* FIXME : we should not be interrupted between next 2 instructions *)
+            Mutex.unlock obj_mutex;
+            Mutex.lock mut (* Let's purposefully lock ourselves ! *)
+        end;
+        
+        (* Then send the result *)
+        let out_ch = out_channel_of_descr fd in
+        output_string out_ch (!obj ^ "\n");
+        flush out_ch
+          
+        | _ -> assert false
+    done
+
+  let server_main () =
+    handle_unix_error (fun () -> 
+      bind server_socket serv_addr;
+      listen server_socket Cfg.port;) ();
+    while true do 
+      let fd = fst =< accept $ server_socket in
+      ignore (fire server_worker fd) (* FIXME : make a real shutdown *)
+    done
+      
 
 
-      (** Interface **)
-      let run = server_main (* FIXME : do we really want the same behavior
-      ? *)
+  (** Interface **)
+  let run = server_main (* FIXME : do we really want the same behavior
+                           ? *)
 
-      let get_addr () = serv_addr
-    end
+  let get_addr () = serv_addr
+end
